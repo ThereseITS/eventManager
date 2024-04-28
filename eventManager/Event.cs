99f58b703d2db7f3305285847668e15a6e691e66
Therese Hume <Therese.Hume@atu.ie>
2024-04-28 14:27:09 +0100

    Add project files.

---
+++ eventManager/Event.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eventManager
+{
+
+    internal class Event
+    {
+        static int eventIdCounter = 1; // Note that a static variable is shared between all class members.
+
+// private class members
+
+        int _eventNumber;
+        string _name;
+        string _eventType;
+        decimal _price;
+        int _capacity;
+        int _seatsSold;
+
+// Properties
+        public string Name { get { return _name; } }
+        public string EventType
+        { 
+            
+            get { return _eventType; }
+
+            set
+            {
+                if ((value.ToLower() != "comedy")&&(value.ToLower()!="drama")&& (value.ToLower() != "concert") && (value.ToLower() != "sporting") )
+                {
+                    throw new ArgumentException("Invalid event type");
+                }
+                else
+                {
+                    _eventType = value;
+                }
+            }
+        }
+    
+        public decimal Price
+        {
+            get { return _price; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Invalid Price");
+                }
+                else
+                {
+                    _price = value;
+                }
+            }
+        }
+        public int Capacity{  
+            
+            get { return _capacity; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Capacity should be greater than or equal to 0");
+                }
+                else
+                {
+                    _capacity = value;
+                }
+            }
+        }
+        public int SeatsSold {  get { return _seatsSold; } } // this value can only be changed by the BuySeats method below.
+
+        // Constructors
+        public Event(string name, string eventType, decimal price, int capacity)
+        {
+            _name = name;
+            EventType = eventType;// use property setters to check input
+            Price = price;
+            Capacity = capacity;
+            _seatsSold = 0;
+            _eventNumber = eventIdCounter++;
+        }
+        
+        public Event(string name, string eventType, decimal price, int capacity, int seatsSold):this(name, eventType,price,capacity) // calls earlier constructor.
+        {
+            if (_seatsSold >= 0)
+            {
+                _seatsSold = seatsSold;
+            }
+            else
+            {
+                throw new ArgumentException("Seats sold should not be less than 0");
+            }
+     
+        }
+
+        //Methods
+
+        /// <summary>
+        /// Privides a classification for the price.
+        /// </summary>
+        /// <returns>This method returns a string  classification for the price - high, low or medium</returns>
+        public string GetPriceClassification()
+        {
+            string classification = "";
+
+            switch(_price)
+            {
+                case decimal p when ((p>=0m) && (p<50m)): classification = "Low";break;
+                case decimal p when ((p >= 50m) && (p < 100m)): classification = "Medium"; break;
+                case decimal p when (p >= 100m): classification = "High"; break;
+                default: classification = "UNKNOWN"; break;
+            }
+
+            return classification;
+        }
+
+        /// <summary>
+        /// Caculates the percentage seats sold.
+        /// </summary>
+        /// <returns>This method returns a double -percentage seats sold - as a value between 0 and 1.</returns>
+        /// 
+        public double CalculatePercentageSeatsSold()
+        {
+            if (_capacity > 0)
+            {
+                return (double)_seatsSold / (double)_capacity;
+            }
+            return 0;
+        }
+/// <summary>
+/// This method returns a formatted string.
+/// </summary>
+/// <returns></returns>
+        public override string ToString()
+        {
+            return $" {_eventNumber,-10} {_name, -15} {_eventType,-10} {_price,-10:C2} {GetPriceClassification(),-10} {_capacity,-10} {_seatsSold,-10} {CalculatePercentageSeatsSold(),-10:P0} sold";
+
+        }
+    }
+}
---
+++ eventManager/Program.cs
@@ -0,0 +1,121 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace eventManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"../../../testData2.csv";
+            List<Event> events = ReadEvents(path);
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            foreach (Event e in events)
+            Console.WriteLine(e.ToString());
+        }
+
+/// <summary>
+/// This method will read in and create a list of events from a file.
+/// Note that a fresh list is created each time. 
+/// </summary>
+/// <param name="path">T</param>
+/// <returns></returns>
+        static List<Event> ReadEvents(string path)
+        {
+            List<Event> events = new List<Event>();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string input;
+                    string name;
+                    string eventType;
+                    decimal price;
+                    int capacity=0;
+                    int seatsSold=0;
+
+                    while ((input = sr.ReadLine()) != null)
+                    {
+                        string[] fields = input.Split(',');
+
+                        // validate the line before creating the object.
+
+                        if (   ((fields.Length == 5 && int.TryParse(fields[4], out seatsSold))
+                            ||  (fields.Length==4) )
+                            && CheckValidType(fields[1])
+                            && decimal.TryParse(fields[2], out price)
+                            && int.TryParse(fields[3], out capacity))
+                            
+                        {
+                            name = fields[0];
+                            eventType = fields[1];
+                            Event ev;
+                            if ((fields.Length == 4))
+                            { 
+                                ev = new Event(name, eventType, price, capacity);
+                            }
+                            else 
+                            {
+                                ev = new Event(name, eventType, price, capacity,seatsSold);
+                            }
+                            
+                            events.Add(ev);
+                        }
+                        else  // Invalid data is reported on but not included in the list. 
+                        {
+                            Console.WriteLine("Error in input: ");
+
+                            for(int i=0;i<fields.Length; i++)
+                            {
+                                Console.WriteLine($"{i}    {fields[i]}");
+                            }
+                        }
+                    }
+                }
+            }
+
+            catch (FileNotFoundException ex)                                            // Throw exceptions in exceptional situations- see Microsoft guidelines.
+            {
+                Console.WriteLine($"File: {path} not found: {ex.Message}");
+            }
+            catch(ArgumentException ex) 
+            {
+                Console.WriteLine($"Error in creating Object - invalid data {ex.Message}");
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return events;
+
+        }
+/// <summary>
+///
+/// This method checks the validity of the event type .
+/// 
+///  **Note that we could alternatively use an enumerated type here & try to parse it.
+///   enum EventType { drama, comedy, sporting, concert}
+///   enum.TryParse(EventType, fields[0], out eventType)
+/// 
+/// 
+/// </summary>
+    /// <param name="eventType"></param>
+    /// <returns>true if valid, false otherwise </returns>
+    public static bool CheckValidType(string eventType)
+        {
+
+            if  ((eventType.ToLower() == "drama")||(eventType.ToLower() == "comedy")|| (eventType.ToLower() == "concert")||(eventType.ToLower() == "sporting"))
+            {
+                return true;
+            }
+            else
+            {
+               
+                return false;
+            }
+        }
+
+    }
+}
